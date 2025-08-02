eea5922a8cee160313d59b385a883c585529cf28
Deepak Maharana <dmaharana@gmail.com>
2025-08-01 21:59:43 -0400

    fix(scripts): resolve server directory path correctly - Changed SERVER_DIR detection to use script's directory location - Added combined cd/pwd command for reliable path resolution
    This addresses potential issues when running the script from different working directories by using `dirname $0` to reliably locate the script's own directory rather than relying on the current working directory (`pwd`).

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # In server
-SERVER_DIR=`pwd`
+SERVER_DIR=${PWD##*/} 
 WEB_DIR="web"
 WEB_PORT=5000
 USER_INTERFACE_REPO="git@github.com:dmaharana/todo_app_react_py_ui.git"
@@ -13,6 +13,6 @@ pnpm build && \
 cp -r dist ../${SERVER_DIR}
 
 # 3. Run server in SERVER_DIR
-cd ${SERVER_DIR} && \
+cd ../${SERVER_DIR} && \
 pip install -r requirements.txt && \
 python api.py --port 5000 --build-dir dist
